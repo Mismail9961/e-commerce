c505989f5b75c0eaa22baea75abb66049ebd7168
Muhammad Ismail <ismailarshad99699@gmail.com>
2026-08-17 16:47:20 +0500

    Also updated the footer contact details

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -105,7 +105,7 @@ We're passionate about elevating your everyday style with premium fashion essent
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
                 </div>
-                <span>+1-234-567-890</span>
+                <span>03360264083</span>
               </a>
               <a 
                 href="mailto:contact@7even86gamehub.pk" 
@@ -116,7 +116,7 @@ We're passionate about elevating your everyday style with premium fashion essent
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
                 </div>
-                <span className="break-all">contact@www.sachchu.store</span>
+                <span className="break-all">sachchupk@gmail.com</span>
               </a>
             </div>
           </div>
